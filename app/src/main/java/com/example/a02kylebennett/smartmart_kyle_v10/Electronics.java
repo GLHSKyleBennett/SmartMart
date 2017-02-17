6e64c91bded226329a80fa60d2cdf9ea81edca0c
Kyle Bennett <02kyle.bennett@gulllakecs.org>
2017-02-17 08:23:12 -0500

    Added Navigation to Electronics

--- app/src/main/java/com/example/a02kylebennett/smartmart_kyle_v10/Electronics.java
+++ app/src/main/java/com/example/a02kylebennett/smartmart_kyle_v10/Electronics.java
@@ -2,10 +2,13 @@ package com.example.a02kylebennett.smartmart_kyle_v10;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,116 +20,85 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Electronics extends AppCompatActivity implements OnItemSelectedListener {
+public class Electronics extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private ListView mDrawerList;
-    private ArrayAdapter<String> mAdapter;
-
-    private ActionBarDrawerToggle mDrawerToggle;
-    private DrawerLayout mDrawerLayout;
-    private String mActivityTitle;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electronics);
-        mDrawerList = (ListView)findViewById(R.id.navList1);
-        addDrawerItems();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        mActivityTitle = getTitle().toString();
-        setupDrawer();
-        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
 
-        // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
-    }
-
-    private void addDrawerItems() {
-        String[] osArray = { "Electronics", "Shop", "Pets", "Food", "Clothing" };
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, osArray);
-        mDrawerList.setAdapter(mAdapter);
-
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Skyline(view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar5);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout5);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
-        });
-    }
-
-    private void setupDrawer() {
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                R.string.drawer_open, R.string.drawer_close) {
 
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle("Navigation!");
-                invalidateOptionsMenu();
-                mDrawerList.bringToFront();
-                mDrawerLayout.requestLayout();
-            }
-
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(mActivityTitle);
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
+
         };
 
-        mDrawerToggle.setDrawerIndicatorEnabled(true);
-        mDrawerLayout.addDrawerListener(mDrawerToggle);
-    }
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view5);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout5);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    //@Override
+    /*public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return true;
+    }*/
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-
-
-        // Activate the navigation drawer toggle
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
+        if (id == R.id.electronics) {
+            Intent intent = new Intent(this, Electronics.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.clothing) {
+            Intent intent = new Intent(this, Clothing.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.food) {
+            Intent intent = new Intent(this, Food_and_Beverages.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.pets) {
+            Intent intent = new Intent(this, Pets.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.all_firms) {
+            Intent intent = new Intent(this, All_Firms.class);
+            startActivity(intent);
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-    public String item = "";
-
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        item = parent.getItemAtPosition(position).toString();
-
-
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
-    public void Search(View view) {
-        Intent intent = new Intent(this, Selector_Spinner.class);
-        Bundle bundle = new Bundle();
-        bundle.putString("Selection", item);
-        intent.putExtras(bundle);
-        startActivity(intent);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout5);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     public void All_Firms(View view) {
